1c4804d6a8d75e45ee53b05ddc84414ecc21e109
Eldr1tchTech <eldr1tchtech.dev@gmail.com>
2026-05-01 14:00:15 -0600

    form is mostly present now

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,11 @@
 import { Hono } from 'hono'
-import { serveStatic } from 'hono/serve-static'
 import { readFile } from 'fs/promises'
+
 const app = new Hono()
 
-// NOTE: Actual Webapp
-
-app.get('/', serveStat,
-  getContent: async (path, c) => c.html(await readFile(path, 'utf8'))ic({
-  path: './index.html'
-}))
+app.get('/', async (c) => {
+  const html = await readFile('./public/index.html', 'utf8')
+  return c.html(html)
+})
 
 export default app
-
-// NOTE: TESTS
-
-// // Basic GET route
-// app.get('/hello', (c) => {
-//   return c.text('Hello Hono!')
-// })
-
-// // Route with a path parameter
-// app.get('/user/:id', (c) => {
-//   const id = c.req.param('id')
-//   return c.json({ userId: id })
-// })
